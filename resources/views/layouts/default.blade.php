ff19348cca2fe0e3ef2b2575bb28c9fb928a8dc3
gdilove <lyly125@live.cn>
2021-07-19 16:08:31 +0800

    切割头部和尾部视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,11 @@
 @yield('head')
 <body>
     @yield('header')
-    @yield('content')
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
